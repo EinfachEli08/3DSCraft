5d363d171b0751c2f62047c0b0048517ee16edd8
Moddimation <vakhdmyt06@gmail.com>
2024-05-12 18:11:17 +0200

    more fancy makefiley

--- include/rendering/TextureMap.h
+++ include/rendering/TextureMap.h
@@ -8,26 +8,27 @@ const int cMipmapLevels = 2;
 const u8 cTextureMapSize	= 128;
 const u8 cTextureTileSize	= 16;
 const u8 cTextureMapTileNum = (cTextureMapSize / cTextureTileSize);
-const u32 cTextureFlags		= GX_TRANSFER_FLIP_VERT(1) | GX_TRANSFER_OUT_TILED(1) | GX_TRANSFER_RAW_COPY(0) |
-						  GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGBA8) |
-						  GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO);
 const C3D_TexInitParams cTextureMapParams =
 	(C3D_TexInitParams){cTextureMapSize, cTextureMapSize, cMipmapLevels, GPU_RGBA8, GPU_TEX_2D, true};
+const u32 cTextureFlags = GX_TRANSFER_FLIP_VERT(1) | GX_TRANSFER_OUT_TILED(1) | GX_TRANSFER_RAW_COPY(0) |
+						  GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGBA8) |
+						  GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO);
 
 class Texture {
 	public:
 		Texture(C3D_Tex* texture, char* filename);
-		Texture(){};
+		Texture() {}
+		~Texture() { delete mTexture; }
 
 		struct MapIcon {
 				uint32_t textureHash;
 				int16_t u, v;
 		};
 
-		C3D_Tex* getTexture() { return &texture; }
+		C3D_Tex* getTexture() { return mTexture; }
 
 	protected:
-		C3D_Tex texture;
+		C3D_Tex* mTexture;
 };
 
 class TextureMap : public Texture {
