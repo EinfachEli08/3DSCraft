9fdfc669b7bde019e69e81fee58c6c55db68a551
vakhdmyt06 <vakhdmyt06@gmail.com>
2024-06-20 20:55:59 +0200

    working json codec handling

--- include/util/JsonIO.h
+++ include/util/JsonIO.h
@@ -2,6 +2,7 @@
 
 #include "client/Exception.h"
 #include <cJSON/cJSON.h>
+#include <cstring>
 #include <string>
 #include <vector/vector.h>
 
@@ -11,23 +12,18 @@ class JsonRead {
 				void* dataRef;
 		};
 
+		vector<JsonValue> mCodec;
+
 		cJSON* loadFromFile(const char* filePath);
 		void parseJsonValue(cJSON* jsonValue, void* variablePtr);
 		void deserializeArray(cJSON* arrayObj, void* arrayPtr);
+		cJSON* getNestedObject(cJSON* root, const char* nestedKey);
 
 	public:
 		void jsonRead(const char* filename);
 
 		template <typename T>
 		void jsonAdd(const char* key, T* dataRef) {
-			JsonValue val;
-			val.key		= strdup(key);
-			val.dataRef = (void*)dataRef;
-			mCodec.push_back(val);
+			mCodec.push_back({key, (void*)dataRef});
 		}
-
-	protected:
-		vector<JsonValue> mCodec;
-
-		cJSON* getNestedObject(cJSON* root, const std::string& nestedKey);
 };
