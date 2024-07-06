abaa036ed2dba647fea73bb6fb5695e99831f846
vakhdmyt06 <vakhdmyt06@gmail.com>
2024-07-06 23:36:14 +0200

    forgor... further enchant + resource script update

--- resources/flip.py
+++ resources/flip.py
@@ -18,7 +18,44 @@ def flip_image(image, input_path, output_dir, slice_number=None):
 def split_and_flip_image(input_path, output_dir):
     # Open the image
     image = Image.open(input_path)
-    
+        
+    # Check if the file is inside the "block/" directory
+    if "block/" in input_path:
+        # Check if the image height exceeds 16 pixels
+        if image.height > 16:
+            # Calculate the number of slices needed
+            num_slices = image.height // 16
+            remainder = image.height % 16
+            if remainder > 0:
+                num_slices += 1
+            print(f"...detected animation of {num_slices} frames...") 
+
+             # Create .mcanim file if there are multiple slices
+            if num_slices is not None and num_slices > 1:
+                filename, _ = os.path.splitext(os.path.basename(input_path))
+                mcanim_file = os.path.join(output_dir, f"{filename}.png.mcanim")
+                print(f"Enchant  {filename}.png.mcanim...")
+                with open(mcanim_file, 'w') as f:
+                    f.write(str(num_slices))
+
+            # Iterate over each slice
+            for i in range(num_slices):
+                # Define the slice bounds
+                top = i * 16
+                bottom = min((i + 1) * 16, image.height)
+
+                # Extract the slice
+                slice_image = image.crop((0, top, image.width, bottom))
+
+                # Flip and save the slice
+                flip_image(slice_image, input_path, output_dir, slice_number=i)
+        else:
+            # Save the original image
+            flip_image(image, input_path, output_dir)
+
+    else:
+        # Save the original image
+        flip_image(image, input_path, output_dir)
     flip_image(image, input_path, output_dir)
 
 
