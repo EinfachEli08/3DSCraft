b51a0636b2c28b0ca3781bf7a5b65c08d7636b62
Team Omega <100494326+EinfachEli08@users.noreply.github.com>
2024-07-16 20:53:28 +0200

    Delete build directory

--- build/Direction.d
+++
@@ -1,4 +0,0 @@
-Direction.o: \
- C:/Users/Elias/CLionProjects/3DSCraft/source/world/Direction.c \
- C:/Users/Elias/CLionProjects/3DSCraft/include/world/Direction.h
-C:/Users/Elias/CLionProjects/3DSCraft/include/world/Direction.h:
