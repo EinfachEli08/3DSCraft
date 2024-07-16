062075524b772ef7313b30489c56c329d56a509d
Team Omega <100494326+EinfachEli08@users.noreply.github.com>
2024-07-16 20:53:28 +0200

    Delete build directory

--- build/VertexFmt.d
+++
@@ -1,6 +0,0 @@
-VertexFmt.o: \
- C:/Users/Elias/CLionProjects/3DSCraft/source/rendering/VertexFmt.c \
- C:/Users/Elias/CLionProjects/3DSCraft/include/rendering/VertexFmt.h \
- C:/Users/Elias/CLionProjects/3DSCraft/include/misc/NumberUtils.h
-C:/Users/Elias/CLionProjects/3DSCraft/include/rendering/VertexFmt.h:
-C:/Users/Elias/CLionProjects/3DSCraft/include/misc/NumberUtils.h:
--- build/lodepng.d
+++
@@ -1,4 +0,0 @@
-lodepng.o: \
- C:/Users/Elias/CLionProjects/3DSCraft/dependencies/lodepng/lodepng.c \
- C:/Users/Elias/CLionProjects/3DSCraft/dependencies/lodepng/lodepng.h
-C:/Users/Elias/CLionProjects/3DSCraft/dependencies/lodepng/lodepng.h:
