6bb4065b9c4001810e451baab02697f1f911a8bc
vakhdmyt06 <vakhdmyt06@gmail.com>
2024-07-19 19:46:48 +0200

    more conflict

--- include/client/renderer/CubeMap.h
+++ include/client/renderer/CubeMap.h
@@ -2,7 +2,6 @@
 
 #include <citro3d.h>
 
-<<<<<<< HEAD
 #include "util/math/VecMath.h"
 
 void CubeMap_Init(int projUniform_);
@@ -10,9 +9,3 @@ void CubeMap_Set(const char* basename, float3 rotation);
 void CubeMap_Deinit();
 
 void CubeMap_Draw(C3D_Mtx* projection, float3 rotationOffset);
-=======
-void CubeMap_Init(int projUniform, const char* basename);
-void CubeMap_Deinit();
-
-void CubeMap_Draw(C3D_Mtx* projection);
->>>>>>> b97ec91dfe0d860b0d6909faad8fb6af6140bb36
